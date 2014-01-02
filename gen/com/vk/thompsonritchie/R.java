8f770fdf99623c78841907f6bb97f04d236ac23b
Vladimir Karagioz <crmrussia@gmail.com>
2014-01-02 13:18:19 +0400

    2nd commit

--- gen/com/vk/thompsonritchie/R.java
+++ gen/com/vk/thompsonritchie/R.java
@@ -22,12 +22,18 @@ public final class R {
     }
     public static final class drawable {
         public static final int dennis_ritchie=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int ken_thompson=0x7f020002;
+        public static final int dennis_ritchie_01=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int ken_thompson=0x7f020003;
+        public static final int ken_thompson01=0x7f020004;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
-        public static final int imageView1=0x7f080000;
+        public static final int action_settings=0x7f080005;
+        public static final int imageView1=0x7f080002;
+        public static final int kenthompsoncaption=0x7f080004;
+        public static final int scrollView1=0x7f080001;
+        public static final int textView1=0x7f080003;
+        public static final int textView2=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -38,9 +44,12 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int dennisritchie=0x7f050006;
+        public static final int dennisritchiebio=0x7f050007;
         public static final int hello_world=0x7f050002;
+        public static final int kenthompson=0x7f050005;
         public static final int ritchie=0x7f050004;
-        public static final int thompson=0x7f050003;
+        public static final int thompsonbio=0x7f050003;
     }
     public static final class style {
         /** 
